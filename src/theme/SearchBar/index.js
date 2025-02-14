41ca762bbc5e3a0ecae304364efa45ef199fd383
Melnik George <melnik.goshan2013@gmail.com>
2025-02-14 18:47:40 +0300

    Update index.js

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import SearchBar from '@theme-original/SearchBar';
 
 export default function CustomSearchBar(props) {
+  console.log('Custom search');
   return <SearchBar {...props} placeholder="Search..."/>;
 }
