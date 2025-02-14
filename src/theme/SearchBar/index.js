1bca66a7d39bfca61b21c29fa18ee6315dcb0e4b
Melnik George <melnik.goshan2013@gmail.com>
2025-02-14 18:41:52 +0300

    Create index.js

---
+++ src/theme/SearchBar/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import SearchBar from '@theme-original/SearchBar';
+
+export default function CustomSearchBar(props) {
+  return <SearchBar {...props} placeholder="Search..."/>;
+}
