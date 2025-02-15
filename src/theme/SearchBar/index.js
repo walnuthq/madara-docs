b34ba74c135c1b389208c7313e4c990c4fe9bc87
Melnik George <melnik.goshan2013@gmail.com>
2025-02-15 17:03:52 +0300

    Modified search bar

--- src/theme/SearchBar/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import SearchBar from '@theme-original/SearchBar';
-
-export default function CustomSearchBar(props) {
-  console.log('Custom search');
-  return <SearchBar {...props} placeholder="Search..."/>;
-}
